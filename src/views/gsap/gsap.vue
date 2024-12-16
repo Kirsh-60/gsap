7dfd4cba2efa4ac77e658571db7e47de3dda4268
刘宁 <7610924+liuning1234561@user.noreply.gitee.com>
2024-12-16 11:26:20 +0800

    迭代

--- src/views/gsap/gsap.vue
+++ src/views/gsap/gsap.vue
@@ -5,11 +5,6 @@
         <div class="box orange"></div>
         <div class="box purple"></div>
         <div class="box green"></div>
-        <div class="box green"></div>
-        <div class="box purple"></div>
-        <div class="box orange"></div>
-        <div class="box purple"></div>
-        <div class="box green"></div>
     </div>
 </template>
 
